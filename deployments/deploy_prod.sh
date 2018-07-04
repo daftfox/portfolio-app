1dd58d041c9a350543051b0512f725124f8de501
Timothy Claassens <tim.claassens@gmail.com>
2018-07-04 23:01:30 +0200

    Updated deployment location

--- deployments/deploy_prod.sh
+++ deployments/deploy_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -arve "ssh $SSH_OPTIONS" --no-perms --no-owner --no-group --delete --exclude '.git' ./dist/ root@$DEPLOY_HOST:/var/www/html/
+rsync -arve "ssh $SSH_OPTIONS" --no-perms --no-owner --no-group --delete --exclude '.git' ./dist/ root@$DEPLOY_HOST:/var/www/portfolio/
